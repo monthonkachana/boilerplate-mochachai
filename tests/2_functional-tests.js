1655380251a6367509d72e4368b71d7079763961
monthonkachana <maxmonthonkachana2@gmail.com>
2023-09-29 09:57:53 +0700

    done

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,24 +1,26 @@
 const chai = require('chai');
 const assert = chai.assert;
-
 const server = require('../server');
-
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
-
 suite('Functional Tests', function () {
   this.timeout(5000);
   suite('Integration tests with chai-http', function () {
     // #1
     test('Test GET /hello with no name', function (done) {
+      // Don't forget the callback...
       chai
-        .request(server)
-        .keepOpen()
-        .get('/hello')
+        .request(server) // 'server' is the Express App
+        .get('/hello') // http_method(url). NO NAME in the query !
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
-          done();
+          // res is the response object
+
+          // Test the status and the text response (see the example above).
+          // Please follow the order -status, -text. We rely on that in our tests.
+          // It should respond 'Hello Guest'
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
+          done(); // Always call the 'done()' callback when finished.
         });
     });
     // #2
@@ -28,42 +30,71 @@ suite('Functional Tests', function () {
         .keepOpen()
         .get('/hello?name=xy_z')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello xy_z');
           done();
         });
     });
     // #3
-    test('Send {surname: "Colombo"}', function (done) {
+    test('send {surname: "Colombo"}', function (done) {
+      // we setup the request for you...
       chai
         .request(server)
-        .keepOpen()
         .put('/travellers')
-
+        /** send {surname: 'Colombo'} here **/
+        .send({ surname: 'Colombo' })
+        // .send({...})
         .end(function (err, res) {
-          assert.fail();
+          /** your tests here **/
+          assert.equal(res.status, 200, 'response status should be 200');
+          assert.equal(res.type, 'application/json', 'Response should be json');
+          assert.equal(
+            res.body.name,
+            'Cristoforo',
+            'res.body.name should be "Christoforo"'
+          );
+          assert.equal(
+            res.body.surname,
+            'Colombo',
+            'res.body.surname should be "Colombo"'
+          );
 
-          done();
+          done(); // Never forget the 'done()' callback...
         });
     });
     // #4
-    test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
+    test('send {surname: "da Verrazzano"}', function (done) {
+      /** place the chai-http request code here... **/
+      //connect server to equal res.
+      chai
+        .request(server)
+        .put('/travellers')
+        .send({ surname: 'da Verrazzano' })
+        /** place your tests inside the callback **/
+        .end(function (err, res) {
+          assert.equal(res.status, 200, 'response status should be 200');
+          assert.equal(res.type, 'application/json', 'Response should be json');
+          assert.equal(res.body.name, 'Giovanni');
+          assert.equal(res.body.surname, 'da Verrazzano');
 
-      done();
+          done();
+        });
     });
   });
 });
 
 const Browser = require('zombie');
-
+Browser.site = 'http://localhost:3000/';
+const browser = new Browser();
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
 
-
+  suiteSetup(function (done) {
+    return browser.visit('/', done);
+  });
 
   suite('Headless browser', function () {
-    test('should have a working "site" property', function() {
+    test('should have a working "site" property', function () {
       assert.isNotNull(browser.site);
     });
   });
@@ -71,15 +102,28 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Colombo').then(() => {
+        browser.pressButton('submit', () => {
+          // assert that status is OK 200
+          browser.assert.success();
+          browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#surname', 'Colombo');
+          browser.assert.elements('span#dates', 1);
+          done();
+        });
+      });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Vespucci').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Amerigo');
+          browser.assert.text('span#surname', 'Vespucci');
+          browser.assert.elements('span#dates', 1);
+          done();
+        });
+      });
     });
   });
 });
